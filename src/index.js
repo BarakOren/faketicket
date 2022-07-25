bbdd40e1ddf9807c5c042b76f738729f75f0ad0c
BarakOren <barakoren5@gmail.com>
2022-07-26 00:04:16 +0300

    secondtest

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-document.getElementById("root").requestFullscreen({ navigationUI: "hide" });
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+document.getElementById("root").requestFullscreen({ navigationUI: "hide" });
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
